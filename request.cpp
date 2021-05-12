fbe9a64070200a2a9b4a062038882b847ea73672
jptr218 <84092766+jptr218@users.noreply.github.com>
2021-05-12 17:40:43 +0100

    Delete request.cpp

--- request.cpp
+++
@@ -1,9 +0,0 @@
-#include "websploit.h"
-
-bool testConnect(string url) {
-    return (exec("curl -s " + url) != "");
-}
-
-string request(string url, string param, string val) {
-    return exec("curl -s -d " + param + "=\"" + val + "\" -X POST " + url);
-}
